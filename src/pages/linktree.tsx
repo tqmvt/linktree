ddd5d8dc8427136623d6ba160e4bc37e2ab5f631
gofixgo <66651502+gofixgo@users.noreply.github.com>
2023-06-09 14:43:32 -0400

    add linktree page

---
+++ src/pages/linktree.tsx
@@ -0,0 +1,46 @@
+import {
+  Link as ChakraLink,
+  Text,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { Footer } from '../components/Footer'
+
+const Index = () => (
+  <Container height="100vh">
+    <Main>
+      <List spacing={3} my={0} color="text">
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          <ChakraLink
+            isExternal
+            href="https://chakra-ui.com"
+            flexGrow={1}
+            mr={2}
+          >
+            Chakra UI <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={CheckCircleIcon} color="green.500" />
+          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+            Next.js <LinkIcon />
+          </ChakraLink>
+        </ListItem>
+      </List>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Cookie Preferences</Text>
+    </Footer>
+  </Container>
+)
+
+export default Index
